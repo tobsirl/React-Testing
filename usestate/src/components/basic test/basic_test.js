657633838bc9bdda15aaeb54a75cd88c2bd57571
Paul Tobin <tobster111@gmail.com>
2020-12-14 16:00:47 +0000

    Basic test using enzyme

---
+++ usestate/src/components/basic test/basic_test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Basic() {
+  return (
+    <div>
+      <h1> Basic Test</h1>
+      <p> This is a basic Test Component</p>
+    </div>
+  );
+}
