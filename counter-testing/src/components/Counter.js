aed56fc081f3c1cbe6fcb9540b03b8632f0a30c9
Paul Tobin <tobster111@gmail.com>
2021-04-21 18:52:39 +0100

    Adding the counter component for testing

--- counter-testing/src/components/Counter.js
+++ counter-testing/src/components/Counter.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react'
 
 export default function Counter() {
+  const [count, setCount] = useState(0)
+
+  function decrement() {
+    setCount((c) => c - 1)
+  }
+
+  function increment() {
+    setCount((c) => c + 1)
+  }
+
+  function reset() {
+    setCount(0)
+  }
   return (
     <div>
-      <h1>Counter</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={reset}>Reset</button>
     </div>
-  );
+  )
 }
