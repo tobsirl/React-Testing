e808e54100fbf31c93d481861117c5ff6ac1a9c5
Paul Tobin <tobster111@gmail.com>
2021-04-21 18:57:17 +0100

    Test for the initial text

--- counter-testing/src/App.test.js
+++ counter-testing/src/App.test.js
@@ -1,4 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen} from '@testing-library/react'
+import App from './App'
 
+describe('tests for the Counter component', () => {
+  it('should check for count: 0', () => {
+    render(<App />)
 
+    const countText = screen.getByRole('heading', {name: /count: 0/i})
+    
+    expect(countText).toBeInTheDocument()
+  })
+
+})
