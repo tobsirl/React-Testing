f2d5668bea348e659ba936c289f3c3e1837057e8
Paul Tobin <tobster111@gmail.com>
2021-04-21 18:45:19 +0100

    Adding prettier to the project

--- counter-testing/src/App.js
+++ counter-testing/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <h1>Counter</h1>
+      <Counter />
     </div>
   );
 }
---
+++ counter-testing/src/components/Counter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Counter() {
+  return (
+    <div>
+      <h1>Counter</h1>
+    </div>
+  );
+}
