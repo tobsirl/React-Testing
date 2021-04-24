1857cb5db3517961a9367ddf648c5434147725e0
Paul Tobin <tobster111@gmail.com>
2021-04-24 19:23:02 +0100

    Added the Counter component

--- react-testing-practice-counter/src/components/Counter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Counter() {
-  return (
-    <div>
-      <h1>My Counter</h1>
-    </div>
-  );
-}
---
+++ react-testing-practice-counter/src/components/Counter/Counter.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useReducer } from 'react';
+
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {
+        count: state.count + parseInt(action.step),
+      };
+    }
+    case 'DECREMENT': {
+      return {
+        count: state.count - action.step,
+      };
+    }
+    case 'RESET': {
+      return {
+        count: 0,
+      };
+    }
+
+    default:
+      throw new Error(`Unsupported type: ${action.type}`);
+  }
+}
+export default function Counter({ initialState = 0 }) {
+  const [state, dispatch] = useReducer(countReducer, { count: initialState });
+  const [step, setStep] = useState(1);
+
+  function increment() {
+    dispatch({ type: 'INCREMENT', step: step });
+  }
+
+  function decrement() {
+    dispatch({ type: 'DECREMENT', step: step });
+  }
+
+  function reset() {
+    dispatch({ type: 'RESET' });
+  }
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setStep(value);
+  }
+
+  const { count } = state;
+
+  return (
+    <div>
+      <h1>My Counter</h1>
+      <h1>{count}</h1>
+
+      <button onClick={decrement}>-</button>
+      <input type="number" value={step} onChange={handleChange} />
+      <button onClick={increment}>+</button>
+      <br />
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
