eeaf99fbb401ed4ee71a3544b6c85099c106ef7e
Paul Tobin <tobster111@gmail.com>
2021-04-25 16:32:40 +0100

    Test for the reset button

--- react-testing-practice-counter/src/components/Counter/__test__/Counter.test.jsx
+++ react-testing-practice-counter/src/components/Counter/__test__/Counter.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import userEvent from '@testing-library/user-event';
+import App from '../../../App';
 
 describe('tests for the counter app', () => {
   it('should have the title of My Counter', () => {
@@ -8,5 +9,27 @@ describe('tests for the counter app', () => {
     const title = screen.getByRole('heading', { name: /my counter/i });
 
     expect(title).toBeInTheDocument();
+  });
+
+  it('should test that the rest button works', () => {
+    // Arrange
+    render(<App />);
+
+    const count = screen.getByRole('heading', { name: /0/i });
+    const plusBtn = screen.getByRole('button', { name: /\+/i });
+    const resetBtn = screen.getByRole('button', { name: /reset/i });
+
+    // click the + button twice
+    userEvent.click(plusBtn);
+    userEvent.click(plusBtn);
+
+    // check for 2
+    expect(count).toHaveTextContent('2');
+
+    // click the reset button
+    userEvent.click(resetBtn);
+
+    // check for 0
+    expect(count).toHaveTextContent('0');
   });
 });
