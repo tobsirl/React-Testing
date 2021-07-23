1329fde96832442893555c64a51972565d9faf09
Paul Tobin <tobster111@gmail.com>
2021-07-23 16:41:01 +0100

    Loading starwars data

--- msw-example/src/components/StarWars.jsx
+++ msw-example/src/components/StarWars.jsx
@@ -4,10 +4,35 @@ export default function StarWars() {
   const [swData, setSwData] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
-  
+
+  async function fetchData() {
+    try {
+      const response = await fetch('https://swapi.dev/api/people')
+      const data = await response.json()
+      setSwData(data.results)
+      setLoading(false)
+    } catch (error) {
+      setError(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (error) {
+    return `Sorry error loading your data ${error.message}`
+  }
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  console.log('swData :>> ', swData)
+
   return (
     <div>
-      <h1>load data</h1>
+      <p>{JSON.stringify(swData)}</p>
     </div>
   )
 }
