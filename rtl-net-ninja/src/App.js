22a9f3182f2bfb359fd69b0849c145048fb5ac37
Paul Tobin <tobster111@gmail.com>
2021-08-03 18:32:59 +0100

    Setup prettier

--- rtl-net-ninja/src/App.js
+++ rtl-net-ninja/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Banner from './components/Banner/Banner';
-import { Switch, Route } from 'react-router-dom';
-import TodoPage from './pages/TodoPage/TodoPage';
-import FollowersPage from './pages/FollowersPage/FollowersPage';
+import logo from './logo.svg'
+import './App.css'
+import Banner from './components/Banner/Banner'
+import { Switch, Route } from 'react-router-dom'
+import TodoPage from './pages/TodoPage/TodoPage'
+import FollowersPage from './pages/FollowersPage/FollowersPage'
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route strict exact path="/followers" component={FollowersPage} />
       </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- rtl-net-ninja/src/App.test.js
+++ rtl-net-ninja/src/App.test.js
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import Header from './components/Header/Header';
+import { render, screen } from '@testing-library/react'
+import Header from './components/Header/Header'
 
 test('renders learn react link', () => {
-  render(<Header title="Todo" />);
+  render(<Header title="Todo" />)
 
-  const linkElement = screen.getByText(/todo/i);
+  const linkElement = screen.getByText(/todo/i)
 
-  expect(linkElement).toBeInTheDocument();
-});
+  expect(linkElement).toBeInTheDocument()
+})
