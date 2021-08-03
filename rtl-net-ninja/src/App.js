234dc5ed05f827d90e11411030d8d7ef1808058f
Paul Tobin <tobster111@gmail.com>
2021-08-03 19:08:22 +0100

    Fixed html imports

--- rtl-net-ninja/src/App.js
+++ rtl-net-ninja/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg'
 import './App.css'
 import Banner from './components/Banner/Banner'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 import TodoPage from './pages/TodoPage/TodoPage'
 import FollowersPage from './pages/FollowersPage/FollowersPage'
 
@@ -9,10 +9,12 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Switch>
-        <Route strict exact path="/" component={TodoPage} />
-        <Route strict exact path="/followers" component={FollowersPage} />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route strict exact path="/" component={TodoPage} />
+          <Route strict exact path="/followers" component={FollowersPage} />
+        </Switch>
+      </Router>
     </div>
   )
 }
