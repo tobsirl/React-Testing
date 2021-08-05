e2d707c90fd03530f00c310754cc9cdf3033e4b9
Paul Tobin <tobster111@gmail.com>
2021-08-05 18:20:29 +0100

    Using a describe for the Header component

--- rtl-net-ninja/src/components/Header/__tests__/Header.test.js
+++ rtl-net-ninja/src/components/Header/__tests__/Header.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from '@testing-library/react'
 import Header from '../Header'
 
-it('should render same text passed into title prop', async () => {
-  render(<Header title="My Header" />)
+describe('tests for the <Header /> component', () => {
+  it('should render same text passed into title prop', async () => {
+    render(<Header title="My Header" />)
 
-  const headingElement = screen.getByText(/my header/i)
+    const headingElement = screen.getByText(/my header/i)
 
-  expect(headingElement).toBeInTheDocument()
+    expect(headingElement).toBeInTheDocument()
+  })
 })
