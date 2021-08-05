ed8361b701fd1fb8b973fd37b79ef98193ab0ea8
Paul Tobin <tobster111@gmail.com>
2021-08-05 18:40:27 +0100

    Using getByRole to target the button

--- rtl-net-ninja/src/components/AddInput/__tests__/AddInput.test.js
+++ rtl-net-ninja/src/components/AddInput/__tests__/AddInput.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import AddInput from '../AddInput'
 
 const mockSetTodo = jest.fn()
@@ -10,5 +10,18 @@ describe('tests for the <AddInput /> component', () => {
     const inputElement = screen.getByPlaceholderText(/add a new task here.../i)
 
     expect(inputElement).toBeInTheDocument()
+  })
+
+  it('should be able to type into input', async () => {
+    render(<AddInput todos={[]} setTodos={mockSetTodo} />)
+
+    const inputElement = screen.getByPlaceholderText(/add a new task here.../i)
+    const addBtn = screen.getByRole('button', { name: /add/i })
+    
+    fireEvent.change(inputElement, { target: { value: 'walk the dog' } })
+    fireEvent.click(addBtn)
+
+
+    expect(inputElement.value).toBe('walk the dog')
   })
 })
