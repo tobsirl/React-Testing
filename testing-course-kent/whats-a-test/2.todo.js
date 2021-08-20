44ac170fb88d82075dc2223df568dc0666a791e2
Paul Tobin <tobster111@gmail.com>
2021-08-20 15:56:59 +0100

    Setup of second todo

---
+++ testing-course-kent/whats-a-test/2.todo.js
@@ -0,0 +1,18 @@
+// writing an assertion library
+/*
+
+Now let's implement our own assertion library.
+Create a function called `expect` that accepts an "actual"
+and returns an object of assertions.
+
+Tip: I want to be able to use it like so:
+
+> expect(actual).toBe(expected)
+
+Then run this code with `node 2.todo`
+
+> Make sure you're in the right directory!
+
+ */
+
+const { sum, subtract } = require('./math');
