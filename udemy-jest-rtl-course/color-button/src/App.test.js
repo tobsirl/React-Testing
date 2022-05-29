863376094cd596b9b1464aaf088e65a8afd7ac8f
Paul Tobin <tobster111@gmail.com>
2022-05-29 12:56:01 +0100

    TTD first test complete

--- udemy-jest-rtl-course/color-button/src/App.test.js
+++ udemy-jest-rtl-course/color-button/src/App.test.js
@@ -2,9 +2,15 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 it('button has correct initial color', () => {
-  
+  render(<App />);
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: /change to blue/i });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 });
 
-it('button has correct initial text', () => {
-  
-});
+it('button has correct initial text', () => {});
+
+it('button turns blue when clicked', () => {});
