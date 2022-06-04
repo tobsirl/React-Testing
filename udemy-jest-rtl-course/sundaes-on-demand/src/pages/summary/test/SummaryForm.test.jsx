e1168094e34b6c9c4a22dd51b4a1628ef0487f6a
Paul Tobin <tobster111@gmail.com>
2022-06-04 11:00:09 +0100

    Mouse over tests

--- udemy-jest-rtl-course/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ udemy-jest-rtl-course/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -32,3 +32,27 @@ it('Checkbox disables button on first click and enables on second click', () => 
 
   expect(confirmBtn).toBeDisabled()
 })
+
+it('popover responds to hover', () => {
+  render(<SummaryForm />)
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i,
+  )
+  expect(nullPopover).not.toBeInTheDocument()
+
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i)
+  userEvent.hover(termsAndConditions)
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i)
+
+  expect(popover).toBeInTheDocument()
+
+  // popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions)
+  const nullPopoverAgain = screen.queryByText(
+    /no ice cream will actually be delivered/i,
+  )
+  expect(nullPopoverAgain).not.toBeInTheDocument()
+})
