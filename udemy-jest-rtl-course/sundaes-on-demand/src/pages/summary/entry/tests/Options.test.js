479382ab17ea43e9309fda65c77b9214b1389599
Paul Tobin <tobster111@gmail.com>
2022-06-05 10:38:05 +0100

    Async means using await and findBy queries

--- udemy-jest-rtl-course/sundaes-on-demand/src/pages/summary/entry/tests/Options.test.js
+++ udemy-jest-rtl-course/sundaes-on-demand/src/pages/summary/entry/tests/Options.test.js
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react'
 import Options from '../Options'
 
-it('displays image for each scoop option from the server', () => {
+it('displays image for each scoop option from the server', async () => {
   render(<Options optionType="scoops" />)
 
   // find images
-  const scoopImages = screen.getAllByRole('img', { name: /scoop$/i })
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i })
 
   expect(scoopImages).toHaveLength(2)
 
