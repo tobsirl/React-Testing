1bcfce79f4d835c2a9202c4c81bcc3c67c140528
Paul Tobin <tobster111@gmail.com>
2022-06-05 10:34:35 +0100

    ScoopOptions

--- udemy-jest-rtl-course/sundaes-on-demand/src/pages/summary/entry/Options.jsx
+++ udemy-jest-rtl-course/sundaes-on-demand/src/pages/summary/entry/Options.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import ScoopOption from './ScoopOption'
+import { Row } from 'react-bootstrap'
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([])
@@ -26,5 +27,5 @@ export default function Options({ optionType }) {
     />
   ))
 
-  return <div>{optionItems}</div>
+  return <Row>{optionItems}</Row>
 }
