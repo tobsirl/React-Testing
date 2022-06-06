0647ff748f574d9df8b524343671bb25b0031c98
Paul Tobin <tobster111@gmail.com>
2022-06-06 11:27:27 +0100

    AlertBanner component

---
+++ udemy-jest-rtl-course/sundaes-on-demand/src/pages/common/AlertBanner.jsx
@@ -0,0 +1,18 @@
+import { Alert } from 'react-bootstrap'
+
+export default function AlertBanner({ message, variant }) {
+  const alertMessage =
+    message || 'An unexpected error occurred. Please try again later.'
+
+  const alertVariant = variant || 'danger'
+
+  return (
+    <Alert
+      key={variant}
+      variant={alertVariant}
+      style={{ backgroundColor: 'red' }}
+    >
+      {alertMessage}
+    </Alert>
+  )
+}
