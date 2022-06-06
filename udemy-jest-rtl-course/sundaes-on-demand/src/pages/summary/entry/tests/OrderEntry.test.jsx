e9f2a0bb48451af3017a07539834046f62b208b8
Paul Tobin <tobster111@gmail.com>
2022-06-06 10:55:13 +0100

    Setup the handlers to return an status 500

--- udemy-jest-rtl-course/sundaes-on-demand/src/pages/summary/entry/tests/OrderEntry.test.jsx
+++ udemy-jest-rtl-course/sundaes-on-demand/src/pages/summary/entry/tests/OrderEntry.test.jsx
@@ -1,6 +1,19 @@
 import { render, screen } from '@testing-library/react'
 import OrderEntry from '../OrderEntry'
+import { rest } from 'msw'
+import { server } from '../../../mocks/server'
 
-it('handles error for scoops and toppings routes ', () => {
+it('handles error for scoops and toppings routes', () => {
+  server.resetHandlers(
+    rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
+      return res(ctx.status(500))
+    }),
+    rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
+      return res(ctx.status(500))
+    }),
+  )
+
+  render(<OrderEntry />)
+
   
-});
+})
