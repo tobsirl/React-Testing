fe5188f6d8c6913fd437c0eff33b48ee0f1e325d
Paul Tobin <tobster111@gmail.com>
2022-06-06 11:22:20 +0100

    Assert on the two errors

--- udemy-jest-rtl-course/sundaes-on-demand/src/pages/summary/entry/tests/OrderEntry.test.jsx
+++ udemy-jest-rtl-course/sundaes-on-demand/src/pages/summary/entry/tests/OrderEntry.test.jsx
@@ -3,7 +3,7 @@ import OrderEntry from '../OrderEntry'
 import { rest } from 'msw'
 import { server } from '../../../mocks/server'
 
-it('handles error for scoops and toppings routes', () => {
+it('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
     rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
       return res(ctx.status(500))
@@ -15,5 +15,9 @@ it('handles error for scoops and toppings routes', () => {
 
   render(<OrderEntry />)
 
-  
+  const alerts = await screen.findAllByRole('alert', {
+    name: /an unexpected error occurred. Please try again later./i,
+  })
+
+  expect(alerts).toHaveLength(2)
 })
