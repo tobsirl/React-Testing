db46d0b6ad02f94135a7a3efec23fc38a9d04e7e
Paul Tobin <tobster111@gmail.com>
2022-06-08 18:34:34 +0100

    useOrderDetailsOrderDetailsProviderOrderDetailsProvider

--- udemy-jest-rtl-course/sundaes-on-demand/src/contexts/OrderDetails.jsx
+++ udemy-jest-rtl-course/sundaes-on-demand/src/contexts/OrderDetails.jsx
@@ -5,4 +5,21 @@ const OrderDetails = createContext()
 // create custom hook to check whether we're inside a provider
 function useOrderDetails() {
   const context = useContext(OrderDetails)
+
+  if (!context) {
+    throw new Error(
+      'useOrderDetails must be used within an OrderDetailsProvider',
+    )
+  }
+
+  return context
+}
+
+function OrderDetailsProvider(props) {
+  // getter: object containing option counts for scoops and toppings, subtotal and totals
+  // setter: updateOptionCount
+  const value = useMemo(() => {
+    return []
+  }, [])
+  return <OrderDetails.Provider value={value} {...props} />
 }
