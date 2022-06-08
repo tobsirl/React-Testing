3cca1b6edeabd4ed2cd5e942bff549d7f8a217e1
Paul Tobin <tobster111@gmail.com>
2022-06-08 18:22:14 +0100

    useOrderDetails

--- udemy-jest-rtl-course/sundaes-on-demand/src/contexts/OrderDetails.jsx
+++ udemy-jest-rtl-course/sundaes-on-demand/src/contexts/OrderDetails.jsx
@@ -1,1 +1,8 @@
 import { createContext, useContext, useState, useMemo } from 'react'
+
+const OrderDetails = createContext()
+
+// create custom hook to check whether we're inside a provider
+function useOrderDetails() {
+  const context = useContext(OrderDetails)
+}
