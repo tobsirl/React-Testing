672929cc053f461754e48dbd2a9db8d2163e385e
Paul Tobin <tobster111@gmail.com>
2022-06-17 16:58:08 +0100

    orderSummaryButton

--- udemy-jest-rtl-course/sundaes-on-demand/src/test/orderPhase.test.jsx
+++ udemy-jest-rtl-course/sundaes-on-demand/src/test/orderPhase.test.jsx
@@ -3,13 +3,30 @@ import userEvent from '@testing-library/user-event'
 
 import App from '../App'
 
-it('order phases for happy path', () => {
+it('order phases for happy path', async () => {
   // render app
+  render(<App />)
   // add ice cream scoops and toppings
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: /vanilla/i,
+  })
   // find and click order button
-  // check summary information based on order
-  // accept terms and conditions and click button on confirm order
-  // confirm order number on confirmation page
-  // click "new order" button on confirmation page
-  // check that scoops and toppings subtotals have been reset
+  userEvent.clear(vanillaInput)
+  userEvent.type(vanillaInput, '1')
+
+  const chocolateInput = screen.getByRole('spinbutton', { name: /chocolate/i })
+  userEvent.clear(chocolateInput)
+  userEvent.type(chocolateInput, '2')
+
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
+    name: /cherries/i,
+  })
+
+  userEvent.click(cherriesCheckbox)
+
+  const orderSummaryButton = screen.getByRole('button', {
+    name: /order sundae/i,
+  })
+
+  userEvent.click(orderSummaryButton)
 })
