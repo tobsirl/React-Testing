da731f9f26fd8bd09b2576d59071c83aa79c10f2
Paul Tobin <tobster111@gmail.com>
2022-06-18 10:53:56 +0100

    Add button to OrderEntry

--- udemy-jest-rtl-course/sundaes-on-demand/src/pages/entry/OrderEntry.jsx
+++ udemy-jest-rtl-course/sundaes-on-demand/src/pages/entry/OrderEntry.jsx
@@ -1,13 +1,15 @@
 import { useOrderDetails } from '../../contexts/OrderDetails'
 import Options from './Options'
+import { Button } from 'react-bootstrap'
 
-export default function OrderEntry() {
+export default function OrderEntry({ setOrderPhase }) {
   const [orderDetails] = useOrderDetails()
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
+      <Button onClick={() => setOrderPhase('review')}>Order Sundae!</Button>
     </div>
   )
 }
