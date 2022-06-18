c6e26e54adcde990eddb548400c9eb2b0e7bdb2a
Paul Tobin <tobster111@gmail.com>
2022-06-18 12:11:03 +0100

    Using a mock to fake props

--- udemy-jest-rtl-course/sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
+++ udemy-jest-rtl-course/sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -17,7 +17,7 @@ it('handles error for scoops and toppings routes', async () => {
     }),
   )
 
-  render(<OrderEntry />)
+  render(<OrderEntry setOrderPhase={jest.fn()} />)
 
   await waitFor(async () => {
     const alerts = await screen.findAllByRole('alert')
